913187aa359edd9e7f9ed2aede322575d9bd9dc5
Svitlana <rdv0011@gmail.com>
2024-03-09 20:42:47 +0100

    feat: implement the basic logic

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,11 +3,27 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -16,31 +32,51 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            Column(modifier = Modifier.fillMaxSize()) {
+              var textState by remember {
+                  mutableStateOf("")
+              }
+
+                val nameListState = remember {
+                    mutableListOf<String>()
                 }
+
+                LazyColumn(modifier = Modifier.fillMaxWidth().weight(1f)){
+                    items(nameListState.size){
+                        Text(text = nameListState[it])
+                    }
+                }
+
+                MyTextField(
+                    textValue = textState,
+                    onValueChanged = {
+                        textState = it
+                    },
+                    onAddClick = {
+                        nameListState.add(textState)
+                        textState = ""
+                    }
+                )
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MyTextField(
+    textValue: String,
+    onValueChanged: (String) -> Unit,
+    onAddClick: ()-> Unit
+) {
+    TextField(
+        value = textValue, onValueChange = {
+            onValueChanged(it)
+        },
+        modifier = Modifier.fillMaxWidth(),
+        trailingIcon = {
+            Icon(imageVector = Icons.Default.Add, contentDescription = null, modifier = Modifier.clickable{ onAddClick()})
+        }
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
